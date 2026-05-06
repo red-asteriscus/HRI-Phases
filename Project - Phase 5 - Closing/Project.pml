38ec93d21bf4062e2db422a292aa9b2aff38bc49
Jo <josee.m.asmar@gmail.com>
2026-05-06 12:45:13 +0300

    p5

--- Project - Phase 5 - Closing/Project.pml
+++ Project - Phase 5 - Closing/Project.pml
@@ -7,6 +7,8 @@
     <Dialogs>
         <Dialog name="ExampleDialog" src="behavior_1/ExampleDialog/ExampleDialog.dlg" />
         <Dialog name="Closing" src="Closing/Closing.dlg" />
+        <Dialog name="speech" src="speech/speech.dlg" />
+        <Dialog name="feedback" src="feedback/feedback.dlg" />
     </Dialogs>
     <Resources>
         <File name="suit picture" src="html/pics/suit_picture.png" />
@@ -20,10 +22,55 @@
         <File name="wlc" src="html/pics/wlc.jpg" />
         <File name="script" src="html/script.js" />
         <File name="style" src="html/style.css" />
+        <File name="all" src="html/css/all.css" />
+        <File name="arrival" src="html/css/arrival.css" />
+        <File name="closing" src="html/css/closing.css" />
+        <File name="diploma_segment" src="html/css/diploma_segment.css" />
+        <File name="feedback" src="html/css/feedback.css" />
+        <File name="formatting" src="html/css/formatting.css" />
+        <File name="home" src="html/css/home.css" />
+        <File name="introducing_guest" src="html/css/introducing_guest.css" />
+        <File name="w3" src="html/css/w3.css" />
+        <File name="welcome" src="html/css/welcome.css" />
+        <File name="all" src="html/js/all.js" />
+        <File name="arrival" src="html/js/arrival.js" />
+        <File name="closing" src="html/js/closing.js" />
+        <File name="diploma_segment" src="html/js/diploma_segment.js" />
+        <File name="displayimage" src="html/js/displayimage.js" />
+        <File name="displayinfo" src="html/js/displayinfo.js" />
+        <File name="fastclick" src="html/js/fastclick.js" />
+        <File name="feedback" src="html/js/feedback.js" />
+        <File name="help" src="html/js/help.js" />
+        <File name="index" src="html/js/index.js" />
+        <File name="introducing_guest" src="html/js/introducing_guest.js" />
+        <File name="jquery" src="html/js/jquery.js" />
+        <File name="qievents" src="html/js/qievents.js" />
+        <File name="vars" src="html/js/vars.js" />
+        <File name="welcome" src="html/js/welcome.js" />
+        <File name="arrival" src="html/pages/arrival.html" />
+        <File name="closing" src="html/pages/closing.html" />
+        <File name="confirmation" src="html/pages/confirmation.html" />
+        <File name="diploma_segment" src="html/pages/diploma_segment.html" />
+        <File name="displayimage" src="html/pages/displayimage.html" />
+        <File name="displayinfo" src="html/pages/displayinfo.html" />
+        <File name="displaytext" src="html/pages/displaytext.html" />
+        <File name="feedback" src="html/pages/feedback.html" />
+        <File name="getinput" src="html/pages/getinput.html" />
+        <File name="help" src="html/pages/help.html" />
+        <File name="introducing_guest" src="html/pages/introducing_guest.html" />
+        <File name="welcome" src="html/pages/welcome.html" />
+        <File name="applause2" src="sounds/applause2.ogg" />
+        <File name="bonuspts" src="sounds/bonuspts.ogg" />
+        <File name="gamepositive" src="sounds/gamepositive.ogg" />
+        <File name="glitch" src="sounds/glitch.ogg" />
+        <File name="intro" src="sounds/intro.ogg" />
+        <File name="itempop" src="sounds/itempop.ogg" />
     </Resources>
     <Topics>
         <Topic name="ExampleDialog_enu" src="behavior_1/ExampleDialog/ExampleDialog_enu.top" topicName="ExampleDialog" language="en_US" />
         <Topic name="Closing_enu" src="Closing/Closing_enu.top" topicName="Closing" language="en_US" />
+        <Topic name="speech_enu" src="speech/speech_enu.top" topicName="speech" language="en_US" />
+        <Topic name="feedback_enu" src="feedback/feedback_enu.top" topicName="feedback" language="en_US" />
     </Topics>
     <IgnoredPaths />
     <Translations auto-fill="en_US">
--- Project - Phase 5 - Closing/html/js/closing.js
+++ Project - Phase 5 - Closing/html/js/closing.js
@@ -1,31 +1,283 @@
-var session = null;
+var activePhase = "closing";
+var closingEventSubscribed = false;
+var toastTimer = null;
+var finalConfettiTimer = null;
 
-if (typeof QiSession === 'undefined') {
-    console.log("Testing locally: loading mock image");
-    var imgTag = document.getElementById("main-image");
-    imgTag.src = "../pics/suit_picture.png";
-    imgTag.style.display = "block";
-} else {
-    // Connect to Pepper's internal memory
-    QiSession(function (s) {
-        session = s;
-        console.log("Connected for Closing!");
-        
+var phaseMap = {
+    "final": {
+        title: "Thank you for celebrating with us",
+        label: "Closing Message",
+        text: "Congratulations, Class of 2026. We wish you success in every next step.",
+        showButtons: false,
+        confetti: true
+    },
+
+    "closing": {
+        title: "Before you leave, we would love your feedback",
+        label: "Survey Introduction",
+        text: "Please take a moment to complete the graduation survey.",
+        showButtons: false,
+        confetti: false
+    },
+
+    "feedback": {
+        title: "Are you ready to meet me at the exit?",
+        label: "Survey Question",
+        text: "",
+        showButtons: true,
+        confetti: false
+    }
+};
+
+function getUrlVars() {
+    var vars = {};
+
+    window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+        vars[key] = decodeURIComponent(value);
+    });
+
+    return vars;
+}
+
+function getUrlParam(param, defaultVal) {
+    var value = defaultVal;
+
+    if (window.location.href.indexOf(param + "=") > -1) {
+        value = getUrlVars()[param];
+    }
+
+    return value;
+}
+
+function getPhaseData(phase) {
+    if (phaseMap[phase]) {
+        return phaseMap[phase];
+    }
+
+    return phaseMap["closing"];
+}
+
+function setText(id, value) {
+    var element = document.getElementById(id);
+
+    if (element) {
+        element.innerHTML = value;
+    }
+}
+
+function addClass(element, className) {
+    if (!element) return;
+
+    if (element.className.indexOf(className) === -1) {
+        element.className = element.className + " " + className;
+    }
+}
+
+function removeClass(element, className) {
+    if (!element) return;
+
+    element.className = element.className
+        .replace(new RegExp("\\b" + className + "\\b", "g"), "")
+        .replace(/  +/g, " ");
+}
+
+function displayPageInformation() {
+    var phase = getUrlParam("phase", "closing");
+    var data = getPhaseData(phase);
+    var body = document.getElementsByTagName("body")[0];
+
+    activePhase = phase;
+
+    if (body) {
+        body.className = "phase-" + phase;
+    }
+
+    setText("question-text", data.title);
+    setText("intro-label", data.label);
+    setText("phase-text", data.text);
+
+    updateButtonState(data);
+    updateConfettiState(data);
+}
+
+function updateButtonState(data) {
+    var groupYesNo = document.getElementById("group-yes-no");
+
+    if (groupYesNo) {
+        groupYesNo.style.display = "none";
+    }
+
+    if (data.showButtons && groupYesNo) {
+        groupYesNo.style.display = "block";
+    }
+}
+
+function handleClosingAnswer(eventName) {
+    if (eventName === "yesAnswer") {
+        makeConfetti(true);
+        return;
+    }
+
+    if (eventName === "noAnswer") {
+        pulsePepper("pulse");
+        return;
+    }
+}
+
+function raiseTabletEvent(eventName, eventValue) {
+    if (typeof raiseEvent === "function") {
+        raiseEvent(eventName, eventValue);
+    }
+}
+
+function onClosingButton(eventName) {
+    raiseTabletEvent(eventName, 1);
+    handleClosingAnswer(eventName);
+}
+
+function subscribeToTabletAnswerEvents() {
+    if (closingEventSubscribed) return;
+    if (typeof QiSession === "undefined") return;
+
+    closingEventSubscribed = true;
+
+    QiSession(function (session) {
         session.service("ALMemory").then(function (memory) {
-            // Listen for image requests
-            memory.subscriber("App/DisplayImage").then(function (subscriber) {
-                subscriber.signal.connect(function (src) {
-                    var imgTag = document.getElementById("main-image");
-                    if (src) {
-                        imgTag.src = "http://198.18.0.1/apps/project-98b01d/" + src;
-                        imgTag.style.display = "block";
-                    } else {
-                        imgTag.style.display = "none";
-                    }
+            memory.subscriber("tabletClosingAnswer").then(function (subscriber) {
+                subscriber.signal.connect(function (value) {
+                    handleClosingAnswer(String(value));
                 });
+            }, function (error) {
+                console.log("Could not subscribe to tabletClosingAnswer:", error);
             });
+        }, function (error) {
+            console.log("Could not access ALMemory:", error);
         });
     }, function () {
-        console.log("Disconnected");
+        console.log("Disconnected from QiSession");
     });
 }
+
+function pulsePepper(className) {
+    var pepper = document.getElementById("pepper-avatar");
+
+    if (!pepper) return;
+
+    removeClass(pepper, "pulse");
+    removeClass(pepper, "wave");
+
+    setTimeout(function () {
+        addClass(pepper, className);
+
+        setTimeout(function () {
+            removeClass(pepper, className);
+        }, 900);
+    }, 20);
+}
+
+function showToast(message) {
+    var toast = document.getElementById("toast");
+
+    if (!toast) return;
+
+    toast.innerHTML = message;
+    removeClass(toast, "show");
+
+    if (toastTimer) {
+        clearTimeout(toastTimer);
+    }
+
+    setTimeout(function () {
+        addClass(toast, "show");
+    }, 20);
+
+    toastTimer = setTimeout(function () {
+        removeClass(toast, "show");
+    }, 1700);
+}
+
+function updateConfettiState(data) {
+    if (data.confetti) {
+        startFinalConfetti();
+        return;
+    }
+
+    stopFinalConfetti();
+}
+
+function startFinalConfetti() {
+    if (finalConfettiTimer) return;
+
+    makeConfetti(false);
+
+    finalConfettiTimer = setInterval(function () {
+        makeConfetti(false);
+    }, 700);
+}
+
+function stopFinalConfetti() {
+    var area = document.getElementById("confetti-area");
+
+    if (finalConfettiTimer) {
+        clearInterval(finalConfettiTimer);
+        finalConfettiTimer = null;
+    }
+
+    if (area) {
+        area.innerHTML = "";
+    }
+}
+
+function makeConfetti(clearExisting) {
+    var area = document.getElementById("confetti-area");
+    var piece;
+    var i;
+
+    if (!area) return;
+
+    if (typeof clearExisting === "undefined") {
+        clearExisting = true;
+    }
+
+    if (clearExisting) {
+        area.innerHTML = "";
+    }
+
+    for (i = 0; i < 32; i++) {
+        piece = document.createElement("span");
+        piece.className = "confetti-piece";
+
+        if (i % 3 === 1) {
+            piece.className += " alt";
+        }
+
+        if (i % 3 === 2) {
+            piece.className += " alt2";
+        }
+
+        piece.style.left = ((i * 41 + 28) % 1120) + "px";
+        piece.style.animationDelay = ((i % 7) * 0.08) + "s";
+
+        area.appendChild(piece);
+        removeConfettiPieceLater(piece);
+    }
+
+    if (clearExisting) {
+        setTimeout(function () {
+            area.innerHTML = "";
+        }, 2200);
+    }
+}
+
+function removeConfettiPieceLater(piece) {
+    setTimeout(function () {
+        if (piece && piece.parentNode) {
+            piece.parentNode.removeChild(piece);
+        }
+    }, 2200);
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+    displayPageInformation();
+    subscribeToTabletAnswerEvents();
+});
