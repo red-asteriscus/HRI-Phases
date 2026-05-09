376dca799f873ae13286300e8d35604f0f3b57a7
Jo <josee.m.asmar@gmail.com>
2026-05-10 00:54:58 +0300

    final cleaning

--- Project - Phase 2 - Welcome Speech/html/js/displayimage.js
+++
@@ -1,6 +0,0 @@
-function displayPageImage() {
-    imageName = getUrlParam("pageImage", "");
-    if (imageName) {
-        document.getElementById("pageImage").src = "../pics/" + imageName;
-    }
-}
